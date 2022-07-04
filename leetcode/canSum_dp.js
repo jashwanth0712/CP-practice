31dd198654daea61f241b65c5a0c83e71f00b7f2
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2022-07-04 23:52:47 +0530

    Create canSum_dp.js
    program to find if the elements in a given array can for the required total sum,

---
+++ leetcode/canSum_dp.js
@@ -0,0 +1,14 @@
+const canSum = (targetSum,numbers,memo={}) =>{
+  if(targetSum in memo) return memo[targetSum];
+  if(targetSum ===0) return true;
+  if(targetSum <0) return false;
+  
+  for(let num of numbers){
+  const remainder = targetSum - num;
+  if(canSum(remainder,numbers,memo)===true){
+    memo[targetSum]= true;
+    return true;
+    }
+  };
+  
+  console.log(canSum(7,[2,3]));
