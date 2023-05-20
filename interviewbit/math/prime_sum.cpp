c650d1839576b8a223439136383437b0f5efb33d
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2023-05-20 23:46:44 +0530

    Create prime_sum.cpp

---
+++ interviewbit/math/prime_sum.cpp
@@ -0,0 +1,8 @@
+  std::vector<bool> myVector(A, true); 
+    for(int i=2;i<sqrt(A);i++){
+        if(A[i]==true){
+            for(int j=2*i;j<sqrt(A;j+=i)){
+                cout<<j
+            }
+        }
+    }
