4a0b01b83e7ffacb7b9c6ef8c6873ecaf83e1d79
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2023-06-19 23:46:50 +0530

    Update Rearrange_array.cpp

--- interviewbit/math/Rearrange_array.cpp
+++ interviewbit/math/Rearrange_array.cpp
@@ -1,1 +1,11 @@
 // Given an array A of size N. Rearrange the given array so that A[i] becomes A[A[i]] with O(1) extra space.
+void Solution:: arrange(vector<int> &A) {
+   int n=A.size();
+   for(int i=0;i<n;i++){
+       A[i]=A[i]+n*(A[A[i]]%n);
+   }
+   for(int i=0;i<n;i++){
+       A[i]/=n;
+   }
+  
+}
